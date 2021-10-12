f12c0c0ac1180c90b739275594358920706ff16b
sararekowska <sara.rekowska@gmail.com>
2021-10-12 22:37:01 +0200

    fix: material-ui errors

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import ApiTest from "./ApiTest.jsx";
+import ApiTest from "./components/ApiTest.jsx";
 
 function App() {
   return (
--- src/components/MovieTable.jsx
+++ src/components/MovieTable.jsx
@@ -5,7 +5,7 @@ import TableCell, { tableCellClasses } from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
+import { Paper } from "@mui/material";
 import { styled } from "@mui/material/styles";
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
